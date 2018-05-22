a6fdaa87621719177b3cc5bfc0b8f2f8d5b9d402
Mariana Veríssimo <mfm.verissimo97@gmail.com>
2018-05-22 23:09:27 +0100

    UbiReact

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 
 const navbar = (
     <nav class="container">
-      <div class="row">
+      <div class="row_nav">
 
-        <div class="col-3">
+        <div class="col-1 hamb">
             <div class="line"></div>
             <div class="line"></div>
             <div class="line"></div>
         </div>
-        <div class="col-4"> <h2>Corum</h2> </div>
+        <div class="col-3 app_name"> <h2>Corum</h2> </div>
+        <div class="col-3"></div>
 
       </div>
     </nav>
@@ -18,14 +19,42 @@ const navbar = (
 
 const title = (
     <div class="container">
-      <div class="row">
+      <div class="row_title">
 
-        <div class="col-3"></div>
-        <div class="col-4"> <h1>Songs</h1> </div>
+        <div class="col-1 blank"></div>
+        <div class="col-5 title"> <h1>Songs</h1> </div>
 
       </div>
     </div>
 );
 
+const song_list = (
+  <div class="container">
+    <div class="row_songList">
+
+      <div class="col-1 blank"></div>
+      <div class="col-9 song_title"> <h3>Title</h3> </div>
+      <div class="col-2 song_artist"> <h4>Artist</h4> </div>
+
+    </div>
+    <div class="row_songList">
+
+      <div class="col-1 blank"></div>
+      <div class="col-9 song_title"> <h3>Title</h3> </div>
+      <div class="col-2 song_artist"> <h4>Artist</h4> </div>
+
+    </div>
+
+    <div class="row_songList">
+
+      <div class="col-1 blank"></div>
+      <div class="col-9 song_title"> <h3>Title</h3> </div>
+      <div class="col-2 song_artist"> <h4>Artist</h4> </div>
+
+    </div>
+  </div>
+);
+
 ReactDOM.render( navbar, document.getElementById("navbar"));
 ReactDOM.render( title, document.getElementById("title"));
+ReactDOM.render( song_list, document.getElementById("song_list"));
