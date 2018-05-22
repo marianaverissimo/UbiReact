ac55fdb4c8519bdbd1d0ea5e7a04312569af01c6
Mariana Veríssimo <mfm.verissimo97@gmail.com>
2018-05-22 12:36:31 +0100

    reactProj

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  React.createElement("div", null, "Hello World"), // <div>Hello World</div>
-  document.getElementById("root")
+const navbar = (
+    <nav class="container">
+      <div class="row">
+
+        <div class="col-3">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <div class="col-4"> <h2>Corum</h2> </div>
+
+      </div>
+    </nav>
 );
+
+const title = (
+    <div class="container">
+      <div class="row">
+
+        <div class="col-3"></div>
+        <div class="col-4"> <h1>Songs</h1> </div>
+
+      </div>
+    </div>
+);
+
+ReactDOM.render( navbar, document.getElementById("navbar"));
+ReactDOM.render( title, document.getElementById("title"));
