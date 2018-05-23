15c1712baae8bc900023a3bac857e412e26a6d53
Mariana Veríssimo <mfm.verissimo97@gmail.com>
2018-05-23 10:53:52 +0100

    UbiReact

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Request from 'react-http-request';
 
 const navbar = (
     <nav class="container">
@@ -28,33 +29,39 @@ const title = (
     </div>
 );
 
+
 const song_list = (
   <div class="container">
     <div class="row_songList">
 
       <div class="col-1 blank"></div>
-      <div class="col-9 song_title"> <h3>Title</h3> </div>
-      <div class="col-2 song_artist"> <h4>Artist</h4> </div>
-
-    </div>
-    <div class="row_songList">
-
-      <div class="col-1 blank"></div>
-      <div class="col-9 song_title"> <h3>Title</h3> </div>
-      <div class="col-2 song_artist"> <h4>Artist</h4> </div>
-
-    </div>
-
-    <div class="row_songList">
-
-      <div class="col-1 blank"></div>
-      <div class="col-9 song_title"> <h3>Title</h3> </div>
+      <div class="col-9 song_title"> <h3>ApiSongs</h3> </div>
       <div class="col-2 song_artist"> <h4>Artist</h4> </div>
 
     </div>
   </div>
 );
 
+const song_one = (
+  <Request
+        url='https://songs-api-ubiwhere.now.sh/api/songs'
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+      {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else {
+              return <div>{ JSON.stringify(result) }</div>;
+            }
+          }
+        }
+        </Request>
+);
+
 ReactDOM.render( navbar, document.getElementById("navbar"));
 ReactDOM.render( title, document.getElementById("title"));
 ReactDOM.render( song_list, document.getElementById("song_list"));
+ReactDOM.render( song_one, document.getElementById("song_one"));
